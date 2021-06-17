42c422d852ab636c6d755dbe96745562caf71bb5
mmazzarino <matheus.mazzarino@acad.pucrs.br>
2021-06-17 02:06:08 -0300

    criacao das declaracoes das funcoes

--- main/main.c
+++ main/main.c
@@ -1,42 +1,115 @@
-#include "freertos/FreeRTOS.h"
-#include "esp_wifi.h"
-#include "esp_system.h"
-#include "esp_event.h"
-#include "esp_event_loop.h"
-#include "nvs_flash.h"
-#include "driver/gpio.h"
+#include <stdbool.h>
 
-esp_err_t event_handler(void *ctx, system_event_t *event)
-{
-    return ESP_OK;
+bool configuraProcessadores();
+bool configuraLora();
+bool configuraIMU();
+bool configuraRMT();
+bool configuraGPS();
+void recebeDadosDaBase();
+void defineEmpuxoDeModoComum();
+void defineAnguloDosEixos();
+void pegaDadosDoIMU();
+void filtraDadosDoIMU();
+void calcula_PID_dosEixos();
+void calculaEmpuxoDeModoDiferencial();
+void totalizaEmpuxosIndividuais();
+void montaFrameDshot();
+void enviaFrameParaESC();
+void pegaGeolocalizacao();
+void enviaDadosParaBase();
+void loopCore1();
+void loopCore2();
+
+
+void app_main(void){
+	configuraProcessadores();
+	configuraLora();
+	configuraIMU();
+	configuraRMT();
+	configuraGPS();
+
 }
 
-void app_main(void)
-{
-    nvs_flash_init();
-    tcpip_adapter_init();
-    ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
-    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-    ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-    ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
-    ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
-    wifi_config_t sta_config = {
-        .sta = {
-            .ssid = CONFIG_ESP_WIFI_SSID,
-            .password = CONFIG_ESP_WIFI_PASSWORD,
-            .bssid_set = false
-        }
-    };
-    ESP_ERROR_CHECK( esp_wifi_set_config(WIFI_IF_STA, &sta_config) );
-    ESP_ERROR_CHECK( esp_wifi_start() );
-    ESP_ERROR_CHECK( esp_wifi_connect() );
+bool configuraProcessadores(){
 
-    gpio_set_direction(GPIO_NUM_4, GPIO_MODE_OUTPUT);
-    int level = 0;
-    while (true) {
-        gpio_set_level(GPIO_NUM_4, level);
-        level = !level;
-        vTaskDelay(300 / portTICK_PERIOD_MS);
-    }
+	return false;
+}
+
+bool configuraLora(){
+
+	return false;
+}
+
+bool configuraIMU(){
+
+	return false;
+}
+
+bool configuraRMT(){
+
+	return false;
+}
+
+bool configuraGPS(){
+
+	return false;
+}
+
+void recebeDadosDaBase(){
+
+}
+
+void defineEmpuxoDeModoComum(){
+
+}
+
+
+void defineAnguloDosEixos(){
+
+}
+
+
+void pegaDadosDoIMU(){
+
+}
+
+void filtraDadosDoIMU(){
+
+}
+
+void calcula_PID_dosEixos(){
+
+}
+
+void calculaEmpuxoDeModoDiferencial(){
+
+}
+
+void totalizaEmpuxosIndividuais(){
+
+}
+
+void montaFrameDshot(){
+
+}
+
+void enviaFrameParaESC(){
+
+}
+
+void pegaGeolocalizacao(){
+
+}
+
+void enviaDadosParaBase(){
+
+}
+
+void loopCore1(){
+
+}
+
+void loopCore2(){
+
 }
 
